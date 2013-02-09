b678bf08b8a98326f74f038e4adc1fed2ebd933b
Michael Coughlin <mcoughli@mines.edu>
2013-02-08 21:03:01 -0700

    Commiting initial part 2 code

---
+++ project03p2/albums.rb
@@ -0,0 +1,12 @@
+﻿#!/usr/bin/env ruby
+require 'sinatra'
+require 'data_mapper'
+require 'dm-sqlite-adapter'
+require_relative 'album'
+
+#DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/albums.sqlite3.db")
+DataMapper.setup(:default, 'sqlite:recall.db')
+set :port, 8080
+get "/" do
+	"Sinatra is working!"
+end
