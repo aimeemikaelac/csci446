0dd97c8a7e22cccafa489182e4caaf06f7d01ab6
Michael Coughlin <mcoughli@mines.edu>
2013-03-03 17:11:02 -0700

    Re-ran scaffolding command with correct options

--- project04/config/initializers/secret_token.rb
+++ project04/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Depot::Application.config.secret_token = '62d3633235b22a9bf35cdcdd68a24dec118f23966733f13c826022dee50f91a0d4a4eb7f84765e2205def3e2a6333927a0da296957e8a3299c120bde7ae79056'
+Depot::Application.config.secret_token = 'b3ca76587eb878834d1208cb05a6e33433f9dc889d1f140bb8e8368097a71fde3584df17fdaa5b50320113ce315698ef4c9ee87bb48e12e81f128e469e60e45c'
