28a39619900c09f37d10f657134c972a14e107ca
Michael Coughlin <mcoughli@mines.edu>
2013-03-04 00:09:35 -0700

    added code to display title of product, add attr_accessible to line_item.rb in LineItem to make tests pass, and fixed syntax errors

--- project04/app/controllers/line_items_controller.rb
+++ project04/app/controllers/line_items_controller.rb
@@ -39,36 +39,38 @@ class LineItemsController < ApplicationController
   end
 
   # POST /line_items
-  # POST /line_items.json
+  # POST /line_items.xml
   def create
-		@cart = current_cart
-		product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build
-		@line_item.product = product
+    @cart = current_cart
+    product = Product.find(params[:product_id])
+    @line_item = @cart.line_items.build(:product => product)
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to(@line_item.cart, notice: 'Line item was successfully created.') }
-        format.json { render json: @line_item, status: :created, location: @line_item }
+        format.html { redirect_to(@line_item.cart,
+          :notice => 'Line item was successfully created.') }
+        format.xml  { render :xml => @line_item,
+          :status => :created, :location => @line_item }
       else
-        format.html { render action: "new" }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @line_item.errors,
+          :status => :unprocessable_entity }
       end
     end
   end
 
   # PUT /line_items/1
-  # PUT /line_items/1.json
+  # PUT /line_items/1.xml
   def update
     @line_item = LineItem.find(params[:id])
 
     respond_to do |format|
       if @line_item.update_attributes(params[:line_item])
-        format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
-        format.json { head :no_content }
+        format.html { redirect_to(@line_item, :notice => 'Line item was successfully updated.') }
+        format.xml  { head :ok }
       else
-        format.html { render action: "edit" }
-        format.json { render json: @line_item.errors, status: :unprocessable_entity }
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @line_item.errors, :status => :unprocessable_entity }
       end
     end
   end
