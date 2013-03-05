7522c9420197f2efc8e6cff80fd5a8c0bc097c8a
Michael Coughlin <mcoughli@mines.edu>
2013-03-04 21:49:34 -0700

    added add_product to line_items_controller.rb

--- project04/app/controllers/line_items_controller.rb
+++ project04/app/controllers/line_items_controller.rb
@@ -86,4 +86,14 @@ class LineItemsController < ApplicationController
       format.json { head :no_content }
     end
   end
+	
+	def add_product(product_id)
+		current_item = line_items.find_by_product_id(product_id)
+		if current_item
+			current_item.quantity += 1
+		else
+			current_item = line_items.build(:product_id => product_id)
+		end
+		current_item
+	end
 end
